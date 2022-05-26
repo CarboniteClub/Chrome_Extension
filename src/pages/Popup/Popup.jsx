b5077932135defdc31b63dd458a24fa1721cf95e
akhil <akhile76@gmail.com>
2022-05-26 23:01:17 +0530

    working local storage

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,25 +3,42 @@ import { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo.svg';
 // import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
-let link = {
-  tst: 0,
-};
 
 const Popup = () => {
   const [url, setUrl] = useState('');
   const [loading, setloading] = useState(true);
+  console.log(localStorage.getItem('link'));
+  let [link, setLink] = useState(0);
   useEffect(() => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       setUrl(tabs[0].url);
+      if (localStorage.getItem('link') == null) {
+        localStorage.setItem('link', 0);
+      } else {
+        link = parseInt(localStorage.getItem('link'));
+      }
+      setLink(link + 1);
+      // console.log(link + 5);
+      localStorage.setItem('link', link + 1);
       setloading(false);
     });
   }, []);
+
+  // let link;
+  // if (link == undefined) {
+  //   link = 0;
+  // } else {
+  //   link++;
+  // }
 
   // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   //   setUrl(tabs[0].url);
   // });
   if (loading) {
     return <div>Loading...</div>;
+  }
+  if (link == undefined) {
+    return <div>Link...</div>;
   }
   return (
     <div className="App">
@@ -31,6 +48,7 @@ const Popup = () => {
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
         <p>{url}</p>
+        <p>{link}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
